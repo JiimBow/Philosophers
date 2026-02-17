2f8a9dc9593b6d9b09bfdd292488c6a30ab9c50b
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-02-17 16:13:08 +0100

    fix gitginore

---
+++ philo/sources/main.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 10:58:13 by jodone            #+#    #+#             */
+/*   Updated: 2026/02/17 15:56:19 by jodone           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <philo.h>
+
+void	*thread_routine(void *data)
+{
+	t_philo	*philo;
+	int		f_left;
+	int		f_right;
+	size_t	timestamp;
+
+	philo = (t_philo *)data;
+	f_left = philo->id;
+	f_right = (philo->id + 1) % philo->data->nb_philo;
+	while (1)
+	{
+		timestamp = get_timestamp(philo);
+		printf("%lu %d is thinking\n", get_timestamp(philo), philo->id + 1);
+		pthread_mutex_lock(&philo->data->fork[f_right]);
+		printf("%lu %d has taken a fork\n", get_timestamp(philo), philo->id + 1);
+		pthread_mutex_lock(&philo->data->fork[f_left]);
+		printf("%lu %d has taken a fork\n", get_timestamp(philo), philo->id + 1);
+		printf("%lu %d is eating\n", get_timestamp(philo), philo->id + 1);
+		pthread_mutex_lock(&philo->meal_mutex);
+		philo->last_meal = get_timestamp(philo);
+		pthread_mutex_unlock(&philo->meal_mutex);
+		usleep(philo->data->eat_time * 1000);
+		philo->nb_meals++;
+		pthread_mutex_unlock(&philo->data->fork[f_left]);
+		pthread_mutex_unlock(&philo->data->fork[f_right]);
+		printf("%lu %d is sleeping\n", get_timestamp(philo), philo->id + 1);
+		usleep(philo->data->sleep_time * 1000);
+		if (philo->data->stop == 1)
+			break ;
+	}
+	return (NULL);
+}
+
+void	*monitor_routine(void *data)
+{
+	t_philo	*monitor;
+	int		i;
+
+	monitor = (t_philo *)data;
+	while (monitor->data->stop == 0)
+	{
+		i = 0;
+		while (i < monitor->data->nb_philo)
+		{
+			pthread_mutex_lock(&monitor->data->stop_mutex);
+			if (get_timestamp(monitor) - monitor[i].last_meal >= monitor->data->starve_time)
+			{
+				printf("%lu %d died\n", get_timestamp(monitor), monitor->id + 1);
+				monitor->data->stop = 1;
+				pthread_mutex_unlock(&monitor->data->stop_mutex);
+				kill_thread(monitor);
+				mutex_destroy(monitor);
+				return (NULL);
+			}
+			pthread_mutex_unlock(&monitor->data->stop_mutex);
+			i++;
+		}
+	}
+	return (NULL);
+}
+
+void	create_philo(t_philo *philo, t_data *data)
+{
+	int			i;
+	pthread_t	monitor;
+
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		pthread_create(&philo[i].thread, NULL, thread_routine, &philo[i]);
+		i++;
+	}
+	pthread_create(&monitor, NULL, monitor_routine, philo);
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		pthread_join(philo[i].thread, NULL);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	t_data	data;
+	t_philo	*philo;
+
+	data.prog_time = 0;
+	if (ac < 5 || ac > 6)
+		return (1);
+	data_init(&data, av);
+	mutex_init(&data);
+	philo = malloc(data.nb_philo * sizeof(t_philo));
+	if (!philo)
+		return (1);
+	philo_init(philo, &data, get_time(philo));
+	create_philo(philo, &data);
+	return (0);
+	// mutex_destroy(&data);
+}
---
+++ philo/sources/utils/clean.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 13:36:17 by jodone            #+#    #+#             */
+/*   Updated: 2026/02/17 15:48:05 by jodone           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <philo.h>
+
+void	mutex_destroy(t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo->data->nb_philo)
+	{
+		pthread_mutex_destroy(&philo->data->fork[i]);
+		i++;
+	}
+	free(philo->data->fork);
+	pthread_mutex_destroy(&philo->meal_mutex);
+	pthread_mutex_destroy(&philo->data->print_mutex);
+	// free(philo);
+}
+
+void	kill_thread(t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo->data->nb_philo)
+	{
+		pthread_detach(philo[i].thread);
+		i++;
+	}
+}
